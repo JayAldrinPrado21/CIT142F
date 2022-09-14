915c22dcf98ec42b05f5f6294634a2ab1ae0f1de
CIT142F-38@M307.UC-BCF.EDU.PH <CIT142F-38@M307.UC-BCF.EDU.PH>
2022-09-14 16:46:52 +0800

    first upload

---
+++ hello.bat
@@ -0,0 +1,8 @@
+@echo off
+git remote set-url origin https://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@github.com/johnnyyespapa21/CIT142F.git
+pause
+
+git add . 
+git commit -m "first upload"
+git push
+pause

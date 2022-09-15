81d10a2a21c8dead18ad2d3faed4796a01e81b47
johnnyyespapa21 <jay.aldrin.prado@gmail.com>
2022-09-15 15:24:50 +0800

    first upload

--- hello.bat
+++ hello.bat
@@ -1,5 +1,5 @@
 @echo off
-git remote set-url origin https://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@github.com/johnnyyespapa21/CIT142F.git
+git remote set-url origin https://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@github.com/johnnyyespapa21/CIT142F.git
 pause
 
 git add . 
